566e58439c1943c85bec298a7a7698f13f9cf62b
Rohan Dosi <rohandosi7@gmail.com>
2017-04-23 17:18:28 +0530

    initial commit

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+import datetime
+from django.utils import timezone
 
 class Document(models.Model):
     docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+
+    def __str__(self):
+        return str(self.docfile)
+
